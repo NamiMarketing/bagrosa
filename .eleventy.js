f5fbd84452edf5712bd62e6bc6a9b9649f591f7c
marcelocho <marcelo.cho@gmail.com>
2025-10-07 15:19:21 -0300

    cleaned base

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/img");
-  eleventyConfig.addPassthroughCopy("./src/admin");
 
-
-  eleventyConfig.addNunjucksFilter("limit", (arr, limit) => {
-    // console.log(arr)
-    return arr?.slice(0, limit);
-  });
 
   return {
     dir: {
